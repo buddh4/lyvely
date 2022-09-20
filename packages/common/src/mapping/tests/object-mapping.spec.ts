eb444406769a8fc089338310451f75625eec53d9
buddh4 <mail@jharrer.de>
2022-09-20 19:38:15 +0200

    Added additional object mapping tests

--- packages/common/src/mapping/tests/object-mapping.spec.ts
+++ packages/common/src/mapping/tests/object-mapping.spec.ts
@@ -1,14 +1,19 @@
 import { mapType, registerMapping } from "../object.mapping";
 import { BaseModel, PropertyType } from "../../model";
 
+class User extends BaseModel<User>{
+  forename: string;
+  lastname: string;
+}
+
+class UserInfos extends BaseModel<UserInfos>{
+  @PropertyType([User])
+  userInfos: User[];
+}
+
 describe('object mapping', () => {
   describe('mapType()', function () {
     it('simple object mapping', async () => {
-      class User extends BaseModel<User>{
-        forename: string;
-        lastname: string;
-      }
-
       class UserInfo extends BaseModel<UserInfo>{
         fullName: string;
       }
@@ -23,16 +28,6 @@ describe('object mapping', () => {
     });
 
     it('array to single object mapping', async () => {
-      class User extends BaseModel<User>{
-        forename: string;
-        lastname: string;
-      }
-
-      class UserInfos extends BaseModel<UserInfos>{
-        @PropertyType([User])
-        userInfos: User[];
-      }
-
       registerMapping([User], UserInfos, (users) => new UserInfos({
         userInfos: users.map(({ forename, lastname }) => ({ forename, lastname }))
       }))
@@ -48,17 +43,18 @@ describe('object mapping', () => {
       expect(userInfo.userInfos[1] instanceof User).toEqual(true);
     });
 
+    it('empty array to single object mapping', async () => {
+      registerMapping([User], UserInfos, (users) => new UserInfos({
+        userInfos: users.map(({ forename, lastname }) => ({ forename, lastname }))
+      }))
+
+      const userInfo = mapType([User], UserInfos, []);
+
+      expect(userInfo.userInfos).toBeDefined();
+      expect(userInfo.userInfos.length).toEqual(0);
+    });
+
     it('single to array object mapping', async () => {
-      class User extends BaseModel<User>{
-        forename: string;
-        lastname: string;
-      }
-
-      class UserInfos extends BaseModel<UserInfos>{
-        @PropertyType([User])
-        userInfos: User[];
-      }
-
       registerMapping(UserInfos,[User], (userInfo) => userInfo.userInfos.map(user => new User(user)))
 
       const users = mapType(UserInfos,[User], new UserInfos({
@@ -71,6 +67,15 @@ describe('object mapping', () => {
       expect(users.length).toEqual(2);
       expect(users[0] instanceof User).toEqual(true);
       expect(users[1] instanceof User).toEqual(true);
+    });
+
+    it('single with empty array to array object mapping', async () => {
+      registerMapping(UserInfos,[User], (userInfo) => userInfo.userInfos.map(user => new User(user)))
+
+      const users = mapType(UserInfos,[User], new UserInfos({userInfos: []}));
+
+      expect(Array.isArray(users)).toEqual(true);
+      expect(users.length).toEqual(0);
     });
   });
 });
