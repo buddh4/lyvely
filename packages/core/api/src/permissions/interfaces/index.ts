83831700af8d57a0069d94bdbb4d509d851323de
buddh4 <mail@jharrer.de>
2023-10-22 21:08:04 +0200

    fix(permissions): Fixed verifyAny / verifyEvery functions in global and profile permission service

--- packages/core/api/src/permissions/interfaces/index.ts
+++ packages/core/api/src/permissions/interfaces/index.ts
@@ -1,3 +1,1 @@
-export * from './permissions.service';
-export * from './permissions-service.interface';
 export * from './global-permissions-service.interface';
--- packages/core/api/src/permissions/interfaces/permissions-service.interface.ts
+++
@@ -1,5 +0,0 @@
-export interface IPermissionsService<TContext> {
-  checkEveryPermission(user: TContext, ...permissions: string[]): Promise<boolean>;
-  checkSomePermission(user: TContext, ...permissions: string[]): Promise<boolean>;
-  checkPermission(user: TContext, permission: string): Promise<boolean>;
-}
