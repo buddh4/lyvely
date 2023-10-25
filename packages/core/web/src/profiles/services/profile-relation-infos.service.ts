ff5e1b02a46c00a77514666d8fc29f82c7979a00
buddh4 <mail@jharrer.de>
2023-10-25 20:43:15 +0200

    feat(profiles): Added cache to profile-relation-info frontend service

--- packages/core/web/src/profiles/services/profile-relation-infos.service.ts
+++ packages/core/web/src/profiles/services/profile-relation-infos.service.ts
@@ -7,6 +7,9 @@ import { useSingleton } from '@lyvely/common';
 import profileRelationsRepository from '@/profiles/repositories/profile-relations.repository';
 import { unwrapAndTransformResponse } from '@/core';
 
+const userInfoCache = new Map<string, ProfileRelationUserInfoModel>();
+const pendingCache = new Map<string, Promise<ProfileRelationUserInfoModel>>();
+
 class ProfileRelationInfosService implements IProfileRelationInfosService {
   async getAllProfileRelationInfos(): Promise<ProfileRelationInfos> {
     return unwrapAndTransformResponse(
@@ -19,10 +22,31 @@ class ProfileRelationInfosService implements IProfileRelationInfosService {
     pid: string,
     uid: string,
   ): Promise<ProfileRelationUserInfoModel> {
-    return unwrapAndTransformResponse(
+    const cacheKey = pid + '_' + uid;
+    const cached = userInfoCache.get(cacheKey);
+    if (cached) return cached;
+
+    const pending = pendingCache.get(cacheKey);
+    if (pending) {
+      return new Promise((resolve, reject) => {
+        pending.then(resolve).catch(reject);
+      });
+    }
+
+    const promise = unwrapAndTransformResponse(
       profileRelationsRepository.getProfileRelationUserInfo(pid, uid),
       ProfileRelationUserInfoModel,
     );
+
+    pendingCache.set(cacheKey, promise);
+
+    try {
+      const result = await promise;
+      userInfoCache.set(cacheKey, result);
+      return result;
+    } finally {
+      pendingCache.delete(cacheKey);
+    }
   }
 }
 
