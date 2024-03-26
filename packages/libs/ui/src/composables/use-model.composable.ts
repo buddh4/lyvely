cd43052cfb48607c83ba7851bf12941e36985c2f
buddh4 <mail@jharrer.de>
2024-03-26 20:31:20 +0100

    feat(ui): Added useModel utility composable to ui lib

---
+++ packages/libs/ui/src/composables/use-model.composable.ts
@@ -0,0 +1,28 @@
+import { ref, watch, Ref, type UnwrapRef } from 'vue';
+import { clone } from '@lyvely/common';
+
+export function useModel<TModel, TModelName extends string = 'modelValue'>(
+  model: TModel,
+  emit: (emit: `update:${TModelName}`, val?: any) => void,
+  propName: TModelName = 'modelValue' as TModelName,
+): { formValue: Ref<UnwrapRef<TModel>> } {
+  const formValue = ref<TModel>(clone(model));
+
+  watch(
+    () => model,
+    (newVal: TModel) => {
+      formValue.value = clone(newVal) as UnwrapRef<TModel>;
+    },
+    { deep: true },
+  );
+
+  watch(
+    () => formValue.value,
+    (newVal) => {
+      emit(`update:${propName}`, newVal);
+    },
+    { deep: true },
+  );
+
+  return { formValue };
+}
