8ad35ea8f2d878df331f0596fada21e5edf03a10
buddh4 <mail@jharrer.de>
2024-05-17 15:23:27 +0200

    fix: Aligned docker build to application build

--- docker/docker-compose-setup.sh
+++ docker/docker-compose-setup.sh
@@ -76,7 +76,7 @@ fi
 if [ "$BUILD" = 1 ]; then
 # Build the web static files which we need for our custom nginx image
   cd ..
-  npm install && npm run common:build && npm run web:build
+  npm install && npx nx run-many -t build
 fi
 
 
