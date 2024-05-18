784d02f85a849a30c5f042b89c86d2a8ac003bec
buddh4 <mail@jharrer.de>
2024-05-18 18:43:52 +0200

    fix: Removed time-series-web and calendar-plan dependency from analytics-web

--- packages/features/analytics/web/tailwind.config.cjs
+++ packages/features/analytics/web/tailwind.config.cjs
@@ -6,7 +6,5 @@ module.exports = {
     ...baseConfig.content,
     require.resolve('@lyvely/ui/tailwind.txt'),
     require.resolve('@lyvely/web/tailwind.txt'),
-    require.resolve('@lyvely/calendar-plan-web/tailwind.txt'),
-    require.resolve('@lyvely/time-series-web/tailwind.txt'),
   ]
 };
