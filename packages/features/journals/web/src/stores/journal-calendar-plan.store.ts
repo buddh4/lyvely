c4530a25bd4e4f0c841c6a4a376fa03dcc2f7706
buddh4 <mail@jharrer.de>
2024-05-27 00:27:50 +0200

    chore: Added remaining packages to rush

--- packages/features/journals/web/src/stores/journal-calendar-plan.store.ts
+++ packages/features/journals/web/src/stores/journal-calendar-plan.store.ts
@@ -6,6 +6,9 @@ import {
   useJournalsClient,
 } from '@lyvely/journals-interface';
 import { useTimeSeriesCalendarPlan } from '@lyvely/time-series-web';
+// TODO: https://github.com/microsoft/TypeScript/issues/47663
+import type {} from 'mitt';
+import type {} from '@lyvely/dates';
 
 export const useJournalPlanStore = defineStore('journal-calendar-plan', () => {
   return useTimeSeriesCalendarPlan<JournalModel, JournalFilter>({
