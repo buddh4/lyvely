4728303fda2d02846b657be500c66d3a39b84155
buddh4 <mail@jharrer.de>
2024-05-30 17:24:06 +0200

    chore: Use of const instead of var in gulpfile

--- packages/core/api/gulpfile.cjs
+++ packages/core/api/gulpfile.cjs
@@ -1,5 +1,5 @@
-var gulp = require('gulp');
-var del = require('del');
+const gulp = require('gulp');
+const del = require('del');
 
 gulp.task('clean', function () {
   return del('dist/**', { force: true });
