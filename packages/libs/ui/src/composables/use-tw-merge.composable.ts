b807976d1212bcd49e0d9a3a8617bf825fe3145b
buddh4 <mail@jharrer.de>
2024-06-04 19:03:34 +0200

    fix(ui): Use of proper twMerge theme config

--- packages/libs/ui/src/composables/use-tw-merge.composable.ts
+++ packages/libs/ui/src/composables/use-tw-merge.composable.ts
@@ -1,5 +1,46 @@
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
 import { computed, type ComputedRef, normalizeClass, useAttrs } from 'vue';
+
+export const lyvelyTwMerge = extendTailwindMerge({
+  extend: {
+    theme: {
+      colors: [
+        'highlight',
+        'main',
+        'sidebar',
+        'dimmed',
+        'inverted',
+        'lyvely',
+        'inverted',
+        'pop',
+        'primary',
+        'primary-light',
+        'primary-dark',
+        'secondary',
+        'secondary-light',
+        'secondary-dark',
+        'warning',
+        'warning-light',
+        'warning-dark',
+        'info',
+        'info-light',
+        'info-dark',
+        'danger',
+        'danger-light',
+        'danger-dark',
+        'success',
+        'success-light',
+        'success-dark',
+      ],
+    },
+    classGroups: {
+      'font-size': ['text-xxs'],
+      'bg-color': ['bg-body', 'bg-main', 'bg-shadow', 'bg-sidebar', 'bg-highlight'],
+      'max-h': ['max-h-1/3', 'max-h-2/3', 'max-h-screen-s'],
+      h: ['h-screen-s'],
+    },
+  },
+});
 
 export const useTwMerge = (
   defaults: unknown
@@ -13,7 +54,7 @@ export const useTwMerge = (
   const originalAttrs = computed(() => ({ ...useAttrs() }));
   const classAttrs = computed(() => normalizeClass(originalAttrs.value.class));
   const classDefaults = computed(() => normalizeClass(defaults));
-  const classResult = computed(() => twMerge(classDefaults.value, classAttrs.value));
+  const classResult = computed(() => lyvelyTwMerge(classDefaults.value, classAttrs.value));
   const attrs = computed(() => ({ ...originalAttrs.value, class: classResult.value }));
 
   return {
