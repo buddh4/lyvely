e4da9c9329f4562ab8ab6fa4dd011b9017812209
buddh4 <mail@jharrer.de>
2024-06-09 21:04:51 +0200

    fix(api): Added user-role.interface to permission index.ts

--- packages/core/interface/src/permissions/interfaces/index.ts
+++ packages/core/interface/src/permissions/interfaces/index.ts
@@ -3,3 +3,4 @@ export * from './permissions-service.interface';
 export * from './global-permissions.interface';
 export * from './visitor-strategy.interface';
 export * from './permission-manager-config.interface';
+export * from './user-role.interface';
