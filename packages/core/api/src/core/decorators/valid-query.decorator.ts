5a7982d544385cb03f4e118ca6235ee56adae6bc
buddh4 <mail@jharrer.de>
2024-06-12 15:43:25 +0200

    feat(api): Added @ValidQuery decorator

---
+++ packages/core/api/src/core/decorators/valid-query.decorator.ts
@@ -0,0 +1,30 @@
+import {
+  Query,
+  type PipeTransform,
+  type Type,
+  ValidationPipe,
+  ValidationPipeOptions,
+} from '@nestjs/common';
+import { omit } from 'lodash';
+
+export interface IValidQueryOptions extends ValidationPipeOptions {
+  pipes?: (Type<PipeTransform> | PipeTransform)[];
+}
+
+export function ValidQuery(): ParameterDecorator;
+export function ValidQuery(property?: string, options?: IValidQueryOptions): ParameterDecorator;
+export function ValidQuery(
+  property?: string | IValidQueryOptions,
+  options?: IValidQueryOptions
+): ParameterDecorator {
+  const bodyProperty = typeof property === 'string' ? property : null;
+  const bodyOptions = typeof property === 'object' ? property : options;
+
+  const pipes = bodyOptions?.pipes || [];
+  pipes.push(new ValidationPipe(omit(bodyOptions, 'pipes')));
+
+  const body = bodyProperty ? Query(bodyProperty, ...pipes) : Query(...pipes);
+  return (target: Object, propertyKey: string | symbol | undefined, parameterIndex: number) => {
+    body(target, propertyKey, parameterIndex);
+  };
+}
