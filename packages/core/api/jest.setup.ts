f9e9998480d5f4668ed6eddaae2bda5e71754f06
buddh4 <mail@jharrer.de>
2024-06-14 18:16:04 +0200

    ci: Added why-is-node-running to investigate open handlers ci unit tests

--- packages/core/api/jest.setup.ts
+++ packages/core/api/jest.setup.ts
@@ -1,4 +1,15 @@
+import log from 'why-is-node-running';
 import 'reflect-metadata';
 import { useDayJsDateTimeAdapter } from '@lyvely/dates';
 
 useDayJsDateTimeAdapter();
+
+afterAll(async () => {
+  //normal cleanup things
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      log();
+      resolve('');
+    }, 500);
+  });
+});
