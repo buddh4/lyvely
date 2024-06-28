f7abe0f4333a7401b87a13e22977e16e5db4fe91
buddh4 <mail@jharrer.de>
2024-06-28 17:45:12 +0200

    chore: Improved user registration module config typing

--- packages/core/api/src/user-registration/user-registration.events.ts
+++ packages/core/api/src/user-registration/user-registration.events.ts
@@ -6,19 +6,17 @@ import {
   USER_REGISTRATION_MODULE_ID,
   UserRegistrationMode,
 } from '@lyvely/interface';
-import { ConfigService } from '@nestjs/config';
+import { LyvelyConfigService } from '@/config';
+import type { UserRegistrationConfig } from './interfaces';
 
 @Injectable()
 export class UserRegistrationEvents {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: LyvelyConfigService<UserRegistrationConfig>) {}
 
   @OnEvent(EVENT_MODULE_APP_CONFIG_ASSEMBLY)
   handleModuleConfigAssembly(event: ModuleAppConfigAssemblyEvent) {
     event.setModuleConfig<IUserRegistrationAppConfig>(USER_REGISTRATION_MODULE_ID, {
-      registrationMode: this.configService.get(
-        'modules.user-registration.mode',
-        UserRegistrationMode.PUBLIC
-      ),
+      registrationMode: this.configService.getModuleConfig('mode', UserRegistrationMode.PUBLIC),
     });
   }
 }
