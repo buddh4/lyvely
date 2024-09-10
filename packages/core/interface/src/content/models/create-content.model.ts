a39d1b36b0a8be00dd62bd62b7e7d3577d0a3074
buddh4 <mail@jharrer.de>
2024-09-10 23:16:05 +0200

    fix: Content title not updated

--- packages/core/interface/src/content/models/create-content.model.ts
+++ packages/core/interface/src/content/models/create-content.model.ts
@@ -2,14 +2,17 @@ import { MaxLength, IsOptional, IsString, IsNotEmpty, Length } from 'class-valid
 import { Trim } from '@lyvely/common';
 import { MAX_CONTENT_TITLE_LENGTH } from '../content.constants';
 import { CreateBaseContentModel } from './create-base-content.model';
+import { Expose } from 'class-transformer';
 
 export class CreateContentModel extends CreateBaseContentModel {
+  @Expose()
   @IsString()
   @IsNotEmpty()
   @Trim()
   @MaxLength(MAX_CONTENT_TITLE_LENGTH)
   title: string;
 
+  @Expose()
   @IsString()
   @IsOptional()
   @Trim()
