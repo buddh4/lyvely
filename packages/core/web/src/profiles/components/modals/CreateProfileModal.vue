3b191f4e2e71fa833600f600d5eb1d55446278f5
buddh4 <mail@jharrer.de>
2024-09-12 14:56:01 +0200

    chore(web): Removed unnecessary import

--- packages/core/web/src/profiles/components/modals/CreateProfileModal.vue
+++ packages/core/web/src/profiles/components/modals/CreateProfileModal.vue
@@ -10,7 +10,7 @@ import {
 } from '@lyvely/interface';
 import { translate } from '@/i18n';
 import { useRouter } from 'vue-router';
-import { profileIdPath, profilePath, profileRoute } from '@/profiles/routes/profile-route.helper';
+import { profileIdPath, profilePath } from '@/profiles/routes/profile-route.helper';
 import { useGlobalPermissions } from '@/common/composables';
 import { isTouchScreen } from '@lyvely/ui';
 
